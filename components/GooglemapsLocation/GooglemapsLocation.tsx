ec23d5ea92f388124c4222d71ccf592fd638193f
Adam <ajhassall2000@gmail.com>
2024-03-12 18:36:25 +0000

    azure

--- components/GooglemapsLocation/GooglemapsLocation.tsx
+++ components/GooglemapsLocation/GooglemapsLocation.tsx
@@ -4,7 +4,6 @@ import classes from './GooglemapsLocation.module.css';
 
 export default function GooglemapsLocation() {
   return (
-    <iframe className={classes.location} title="OurLocation" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.729390790609!2d-1.4606795850758007!3d53.38112987993578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487962754417c98d%3A0x382764a176db8b70!2sSheffield%2C%20UK!5e0!3m2!1sen!2sus!4v1677445850449!5m2!1sen!2sus" width="600" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
-
+    <iframe className={classes.location} title="OurLocation" src="https://www.google.com/maps/d/embed?mid=11i43HyMHgsNqNuNKpTfPJyC_v2r6Bko&ehbc=2E312F&noprof=1" width="640" height="480" />
   );
 }
